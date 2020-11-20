91001133e27628ab3f2c59a34f4d11680c88b323
jjkim <objects76@gmail.com>
2020-11-20 13:41:57 +0900

    # write 15MB is NG

--- Devtools.js
+++ Devtools.js
@@ -19,18 +19,56 @@ export function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-export function buildArrayBuffer(n, seed) {
-  const buffer = new ArrayBuffer(n);
-  const intptr = new Uint8Array(buffer);
-  for (let i = 0; i < intptr.length; ++i, ++seed) intptr[i] = seed % 256;
+export function getByteSize(n) {
+  if (n instanceof Blob) n = n.size;
+  if (n < 1024) return n + " B";
+  if (n < 1024 * 1024) return (n / 1024).toFixed(2) + " KB";
+  if (n < 1024 * 1024 * 1024) return (n / 1024 / 1024).toFixed(2) + " MB";
+  return (n / 1024 / 1024 / 1024).toFixed(2) + " GB";
+}
 
-  return { nextSeed: seed, buffer };
+export function buildArrayBuffer(n, seed) {
+  console.assert(n >= 4);
+  const buffer = new ArrayBuffer(n);
+  new Uint32Array(buffer)[0] = n;
+  const cptr = new Uint8Array(buffer, 4);
+  for (let i = 0; i < cptr.length; ++i) cptr[i] = i % 256;
+
+  return { nextSeed: 0, buffer };
 }
 
 const check = (intptr, seed) => {
   for (let i = 0; i < intptr.length; ++i, ++seed) {
     if (intptr[i] !== seed % 256) {
-      console.error(`[${i}]: expect ${seed % 256}, but ${intptr[i]}`);
+      console.error(`[${i}/${getByteSize(i)}]: expect ${seed % 256}, but ${intptr[i]}`);
+      return false;
+    }
+  }
+  return true;
+};
+
+const checkChunk = (arrbuf, seed) => {
+  const intptr = new Uint32Array(arrbuf);
+  const n = intptr[0] - 4;
+
+  for (let offset = 0; offset < arrbuf.byteLength; ) {
+    const chunksize = new Uint32Array(arrbuf, offset)[0];
+    const cptr = new Uint8Array(arrbuf, offset + 4, chunksize - 4);
+    offset += chunksize;
+
+    for (let i = 0; i < cptr.length; ++i) {
+      if (cptr[i] !== i % 256) {
+        console.error(`[${i}/${getByteSize(i)}]: expect ${seed % 256}, but ${intptr[i]}`);
+        return false;
+      }
+    }
+  }
+
+  const cptr = new Uint8Array(arrbuf);
+
+  for (let i = 0; i < intptr.length; ++i, ++seed) {
+    if (intptr[i] !== seed % 256) {
+      console.error(`[${i}/${getByteSize(i)}]: expect ${seed % 256}, but ${intptr[i]}`);
       return false;
     }
   }
@@ -43,26 +81,47 @@ export async function checkBuffer(buf, seed) {
       const blobReader = new FileReader();
       blobReader.onabort = () => ng(blobReader.error);
       blobReader.onload = () => {
-        const intptr = new Uint8Array(blobReader.result);
-        ok(check(intptr, seed));
+        ok(checkChunk(blobReader.result, seed));
       };
       blobReader.readAsArrayBuffer(buf);
       console.log(`state=${blobReader.readyState}`);
     } else if (buf instanceof ArrayBuffer) {
-      ok(check(new Uint8Array(buf), seed));
+      ok(checkChunk(new Uint8Array(buf), seed));
     } else {
       throw new Error("invalid buffer type");
     }
   });
 }
 
-export async function checkLinear(blob) {
-  return new Promise((ok) => {
-    const blobReader = new FileReader();
-    blobReader.onload = () => {
-      const intptr = new Uint8Array(blobReader.result);
-      ok(check(intptr, intptr[0]));
-    };
-    blobReader.readAsArrayBuffer(blob);
-  });
+// export async function checkLinear(blob) {
+//   return new Promise((ok) => {
+//     const blobReader = new FileReader();
+//     blobReader.onload = () => {
+//       const intptr = new Uint8Array(blobReader.result);
+//       ok(check(intptr, intptr[0]));
+//     };
+//     blobReader.readAsArrayBuffer(blob);
+//   });
+// }
+
+export async function getAt(buf, idx) {
+  if (buf instanceof Blob) {
+    return new Promise((ok, ng) => {
+      const blobReader = new FileReader();
+      blobReader.onabort = () => ng(blobReader.error);
+      blobReader.onload = () => {
+        const intptr = new Uint8Array(blobReader.result);
+        console.log(`buf[${idx} = ${intptr[idx]}`);
+        ok(intptr[idx]);
+      };
+    });
+    blobReader.readAsArrayBuffer(buf);
+    console.log(`state=${blobReader.readyState}`);
+  } else if (buf instanceof ArrayBuffer) {
+    const intptr = new Uint8Array(buf);
+    console.log(`buf[${idx} = ${intptr[idx]}`);
+    Promise.resolve(intptr[idx]);
+  } else {
+    throw new Error("invalid buffer type");
+  }
 }
